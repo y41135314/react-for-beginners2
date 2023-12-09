f74203fcfd02b3fc0220911f3bacf0df97c7c20f
y41135314 <y41135314@naver.com>
2023-12-09 17:47:47 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
-<Router basename={process.env.PUBLIC_URL} />
-
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL} >
       <Routes>
         <Route path="/"       element={<Home />} /> 
         <Route path="/movie/:id"  element={<Detail />} /> {/* :id 는 useParams 을 호출했을 때, key값으로 id가 들어오고 value 값이 들어올 것을 의미. */}
